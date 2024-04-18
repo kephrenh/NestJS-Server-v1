fc6ffacaf9233a917577904c524f1ace115e948b
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-04-18 22:59:38 +0200

    setup users controller skeleton

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+// type EmptyObj = Record<PropertyKey, never>
+
+@Controller('users') // /users
+export class UsersController {
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'USER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: Record<string, never>) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: Record<string, never>) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
